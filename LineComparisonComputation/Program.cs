282b9407894f62c1f080bc16479b4e216b3db996
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-05 14:28:40 +0530

    calculating the length of a line

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -3,5 +3,16 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Line Comparison Computation Program");
+        int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+        double length = 0;
+        Console.WriteLine("Enter the value of x1 and x2 ");
+        x1 = Convert.ToInt32(Console.ReadLine());
+        x2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the value of y1 and y2 ");
+        y1 = Convert.ToInt32(Console.ReadLine());
+        y2 = Convert.ToInt32(Console.ReadLine());
+        //calculating the length of line
+        length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+        Console.WriteLine("Length of a Line "+length);
     }
 }
