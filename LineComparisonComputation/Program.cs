7ca4a87e40c439efac10e48487888ea016f165de
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-10 20:58:41 +0530

    using class compare line length and point as well as equal and compareTo

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -1,10 +1,9 @@
 ﻿internal class Program
 {
-    static void Main(string[] args)
+    public static int x1 = 0, x2 = 0, y1 = 0, y2 = 0, a1 = 0, a2 = 0, b1 = 0, b2 = 0;
+    public static double length = 0, length2 = 0;
+    public static void input()
     {
-        Console.WriteLine("Welcome to Line Comparison Computation Program");
-        int x1 = 0, x2 = 0, y1 = 0, y2 = 0, a1 = 0, a2 = 0, b1 = 0, b2 = 0;
-        double length = 0,length2 = 0;
         Console.WriteLine("Enter the value of x1 and x2 ");
         x1 = Convert.ToInt32(Console.ReadLine());
         x2 = Convert.ToInt32(Console.ReadLine());
@@ -20,11 +19,15 @@
         //calculating the length of lines
         length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
         length2 = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
-        Console.WriteLine("Length of First Line "+length);
+        Console.WriteLine("Length of First Line " + length);
         Console.WriteLine("Lenth of Second Line " + length2);
+        
+    }
+    public static void checkEquality()
+    {
         //checking equality of two lines 
         bool line = length.Equals(length2);
-        if (line == true )
+        if (line == true)
         {
             Console.WriteLine("Two lines are equal");
         }
@@ -32,6 +35,10 @@
         {
             Console.WriteLine("Two lines are not equal");
         }
+        
+    }
+    public static void compare()
+    {
         //comparing length of two lines
         double difference = length.CompareTo(length2);
         if (difference < 0)
@@ -46,6 +53,12 @@
         {
             Console.WriteLine("Length of two lines are equal");
         }
-
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Line Comparison Computation Program");
+        input();
+        checkEquality();
+        compare();
     }
 }
